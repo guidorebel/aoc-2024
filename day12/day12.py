7b33df074a6bba142670e553e2a25c5d41bb1356
gdrbl <github@guidorebel.nl>
2024-12-12 23:40:31 +0100

    AOC 2024 - day 12 part 2

--- day12/day12.py
+++ day12/day12.py
@@ -25,53 +25,86 @@ class DataGrid():
         else: return self.value_outofbounds
 
 
-def growRegion (grid, region, start):
+
+def growRegion (grid, plant, start):
 
     tovisit = set()
     visited = set()
     
     area = 0
     perimeter = 0
+    sides = 0
 
     # Add the start coordinate to the list
     tovisit.add(start)
 
-    # Search the coordinates for the same value
+    # Search the coordinates for the expected plant value
     while len(tovisit) > 0:
 
         x, y = tovisit.pop()
 
-        r = grid.get(x, y)
-        if r == region:
+        if grid.get(x, y) == plant:
 
             area += 1
-            visited.add ( (x,y) )
+            visited.add ((x,y))
 
+            # Search the neighbouring coordinates for the expected plant value
             for dx, dy in dirs:
-                if grid.get(x+dx, y+dy) == region:
+                if grid.get(x+dx, y+dy) == plant:
                     if not (x+dx,y+dy) in visited:
+                        # Add this coordinate to the list
                         tovisit.add((x+dx, y+dy))
                 else:
                     perimeter += 1
 
-    price = area * perimeter
-    print (f"A region of {region} plants with price {area} * {perimeter} = {price}")
 
-    return visited, price
+    # Count the nr of sides, which is equal to the nr of corners.
+    # Corners can be found by checking neighbouring values.
+    # 
+    # .  .  .
+    # .  x  x    the x in the middle has two outer corners
+    # .  .  .
+    #
+    # .  x  .
+    # x  x  x    the x in the middle has four inner corners
+    # .  x  .
+
+    for x, y in visited:
+
+        # Outer corners have two sides that are not in the visited region
+        if (x-1, y) not in visited and (x, y-1) not in visited: sides += 1
+        if (x-1, y) not in visited and (x, y+1) not in visited: sides += 1
+        if (x+1, y) not in visited and (x, y-1) not in visited: sides += 1
+        if (x+1, y) not in visited and (x, y+1) not in visited: sides += 1
+
+        # Inner corners have two sides that are in the visited region, and the opposite coordinate is not in the visited region
+        if (x-1, y) in visited and (x, y-1) in visited and (x-1, y-1) not in visited: sides += 1
+        if (x-1, y) in visited and (x, y+1) in visited and (x-1, y+1) not in visited: sides += 1
+        if (x+1, y) in visited and (x, y-1) in visited and (x+1, y-1) not in visited: sides += 1
+        if (x+1, y) in visited and (x, y+1) in visited and (x+1, y+1) not in visited: sides += 1
+
+    return visited, area, perimeter, sides
 
 
 
 grid = DataGrid ("day12\\input.txt")
 
-
 visited = set()
-price = 0
+
+price1 = 0
+price2 = 0
 
 for y in range (grid.rows):
     for x in range (grid.cols):
         if not (x,y) in visited:
-            v, p = growRegion(grid, grid.get (x,y), (x,y))
-            visited.update (v)
-            price += p
+            v, a, p, s = growRegion(grid, grid.get (x,y), (x,y))
 
-print (price)
+            p1 = a * p     # print (f"A region of {grid.get (x,y)} plants with price {a} * {p} = {p1}")
+            p2 = a * s     # print (f"A region of {grid.get (x,y)} plants with price {a} * {s} = {p2}")
+
+            visited.update (v)
+            price1 += p1
+            price2 += p2
+
+print (f"Part 1: {price1}")
+print (f"Part 2: {price2}")
